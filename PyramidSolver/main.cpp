6a5bb2e0fdbeea1961b6c126dcb86dad865661fc
Diego Munoz Arroyo <diego.munoz.arr@gmail.com>
2024-10-24 16:19:11 -0700

    Implementation of the pyramid structure

--- PyramidSolver/main.cpp
+++ PyramidSolver/main.cpp
@@ -6,9 +6,24 @@
 //
 
 #include <iostream>
+#include <vector>
+
+using namespace std;
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    vector<vector<int>> pyramid = {
+            {2},
+            {4, 3},
+            {3, 2, 6},
+            {2, 9, 5, 2},
+            {10, 5, 2, 15, 5}
+        };
+    
+    for (const auto& row : pyramid) {
+            for (int num : row) {
+                cout << num << " ";
+            }
+            cout << endl;
+        }
     return 0;
 }
